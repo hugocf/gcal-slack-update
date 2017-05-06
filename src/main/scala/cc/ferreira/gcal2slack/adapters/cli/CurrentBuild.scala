c6e291189018c5a63e4f2b3853fc424b050ec9bd
Hugo Ferreira <hugo@ferreira.cc>
2017-05-06 21:29:30 +0100

    cli: No need to override what is not implemented

--- src/main/scala/cc/ferreira/gcal2slack/adapters/cli/CurrentBuild.scala
+++ src/main/scala/cc/ferreira/gcal2slack/adapters/cli/CurrentBuild.scala
@@ -6,12 +6,12 @@ import java.time.{LocalDate, LocalDateTime}
 import cc.ferreira.gcal2slack.adapters.cli.core.BuildInfo
 
 object CurrentBuild extends BuildInfo {
-  override val name: String = buildinfo.BuildInfo.name
-  override val version: String = buildinfo.BuildInfo.version
-  override val scalaVersion: String = buildinfo.BuildInfo.scalaVersion
-  override val sbtVersion: String = buildinfo.BuildInfo.sbtVersion
+  val name: String = buildinfo.BuildInfo.name
+  val version: String = buildinfo.BuildInfo.version
+  val scalaVersion: String = buildinfo.BuildInfo.scalaVersion
+  val sbtVersion: String = buildinfo.BuildInfo.sbtVersion
 
-  override def buildDate: LocalDate = {
+  def buildDate: LocalDate = {
     val epoch = buildinfo.BuildInfo.builtAtMillis / 1000
     LocalDateTime.ofEpochSecond(epoch, 0, UTC).toLocalDate
   }
