3e28e1192a7a3e966ac03eb654b5017bda91c398
Hugo Ferreira <hugo@ferreira.cc>
2018-12-28 20:00:45 +0000

    cli: Show help when there are no valid args

--- src/main/scala/cc/ferreira/gcal2slack/cli/CommandLine.scala
+++ src/main/scala/cc/ferreira/gcal2slack/cli/CommandLine.scala
@@ -5,10 +5,13 @@ import java.io.File
 import cc.ferreira.gcal2slack.buildinfo.BuildInfo
 
 object CommandLine {
-  def parseActions(args: Seq[String])(implicit info: BuildInfo): Seq[Action] =
-    args.map {
+  def parseActions(args: Seq[String])(implicit info: BuildInfo): Seq[Action] = {
+    val actions = args.map {
       case "--help" => ShowHelp
       case "--version" => ShowVersion(info)
+      case s if s.startsWith("-") => ShowHelp
       case filename => ReadFile(new File(filename))
     }
+    if (actions.isEmpty) Seq(ShowHelp) else actions
+  }
 }
--- src/test/scala/cc/ferreira/gcal2slack/cli/CommandLineSpec.scala
+++ src/test/scala/cc/ferreira/gcal2slack/cli/CommandLineSpec.scala
@@ -3,9 +3,29 @@ package cc.ferreira.gcal2slack.cli
 import cc.ferreira.gcal2slack.BaseSpec
 import cc.ferreira.gcal2slack.buildinfo.{BuildInfo, CurrentBuild}
 
+import scala.util.Random._
+
 class CommandLineSpec extends BaseSpec {
 
   "parsing actions" - {
+    "show help message when there are no args" in {
+      val args = List.empty
+
+      val result = CommandLine.parseActions(args)
+
+      result should have size 1
+      result.head shouldBe ShowHelp
+    }
+
+    "show help message if there is an unknown option" in {
+      val args = List(s"-${nextString(nextInt(10))}")
+
+      val result = CommandLine.parseActions(args)
+
+      result should have size 1
+      result.head shouldBe ShowHelp
+    }
+
     "--help option should return a help action" in {
       val args = List("--help")
 
