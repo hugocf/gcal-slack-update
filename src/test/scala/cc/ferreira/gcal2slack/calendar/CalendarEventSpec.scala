9bacc30bd5c7986005af028f9c99d764b460b611
Hugo Ferreira <hugo@ferreira.cc>
2019-12-15 00:17:26 +0000

    app: Remove warning about forward references

--- src/test/scala/cc/ferreira/gcal2slack/calendar/CalendarEventSpec.scala
+++ src/test/scala/cc/ferreira/gcal2slack/calendar/CalendarEventSpec.scala
@@ -8,32 +8,34 @@ class CalendarEventSpec extends BaseSpec {
 
   "apply" - {
     "should allow events with 0 time length" in {
-      noException should be thrownBy CalendarEvent("test", t, t)
+      noException should be thrownBy CalendarEvent("test", t.now, t.now)
     }
 
     "should fail creating events with an invalid time interval" in {
       an [IllegalArgumentException] should be thrownBy
-        CalendarEvent("test", t.plusHours(1), t.minusHours(1))
+        CalendarEvent("test", t.now.plusHours(1), t.now.minusHours(1))
     }
   }
 
   "all day property" - {
     "should be true for all day events" in {
-      CalendarEvent("test", t.toLocalDate) should be an 'allDay
+      CalendarEvent("test", t.now.toLocalDate) should be an 'allDay
     }
 
     "should be false" - {
       "for simple regular events" in {
-        val allDay = CalendarEvent("test", t.minusHours(1), t.plusHours(1))
+        val allDay = CalendarEvent("test", t.now.minusHours(1), t.now.plusHours(1))
         CalendarEvent("test", allDay.begin, allDay.end) should not be 'allDay
       }
 
       "for regular events spanning an entire day" in {
-        val allDay = CalendarEvent("test", t.toLocalDate)
+        val allDay = CalendarEvent("test", t.now.toLocalDate)
         CalendarEvent("test", allDay.begin, allDay.end) should not be 'allDay
       }
     }
   }
 
-  private val t = LocalDateTime.now
+  private object t {
+    val now: LocalDateTime = LocalDateTime.now
+  }
 }
